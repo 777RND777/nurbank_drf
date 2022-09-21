70a398d5fe0997dfa259ca5d148460e427d17468
Nurmukhanbet Rakhimbayev <rakhimbayev.n@source.tnt>
2022-09-21 23:51:24 +0600

    basic get request was added

---
+++ nurbank_api_drf/bank/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+from django.utils.text import slugify
+
+
+class User(AbstractUser):
+    slug = models.SlugField(max_length=150, default="admin")
+    debt = models.IntegerField(default=0)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.username
+
+
+class Application(models.Model):
+    value = models.IntegerField()
+    request_date = models.DateTimeField(auto_now_add=True)
+    answer_date = models.DateTimeField(blank=True, null=True)
+    approved = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="applications", null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    class Meta:
+        ordering = ["-pk"]
