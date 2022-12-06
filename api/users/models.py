fe73c13fc6d7a2c633f2ec2a62c17473a9b35341
Nurmukhanbet Rakhimbayev <rakhimbayev.n@source.tnt>
2022-12-07 02:08:50 +0600

    unnecessary custom UserManager was removed

--- api/users/models.py
+++ api/users/models.py
@@ -1,28 +1,15 @@
-from django.contrib.auth.models import AbstractUser, UserManager as Manager
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.text import slugify
-
-
-class UserManager(Manager):
-    def create_user(self, username, password=None, **extra_fields):
-        user = self.model(username=username, **extra_fields)
-        user.slug = slugify(username)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        user = self.create_user(username=username, password=password, **extra_fields)
-        return user
 
 
 class User(AbstractUser):
     slug = models.SlugField(max_length=150)
     debt = models.IntegerField(default=0)
 
-    objects = UserManager()
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.username)
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.username
